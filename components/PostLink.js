fb582fb8eba5fc24466ba4de9a1b85c539be9738
Kryptic <rcm@hypervise.tech>
2020-01-01 10:21:46 -0700

    Initial Next App test

---
+++ components/PostLink.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+
+const PostLink = ({ post }) => (
+    <li>
+        <Link href="/p/[id]" as={`/p/${post.id}`}>
+            <a>{post.title}</a>
+        </Link>
+        <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+    </li>
+);
+
+export default PostLink;
