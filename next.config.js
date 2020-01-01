3ef6b266761506ab3e5ade58aef2565e9e10e243
Kryptic <rcm@hypervise.tech>
2020-01-01 10:33:03 -0700

    Updated serving

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 // next.config.js
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
-    /* config options here */
+    exportPathMap: function() {
+        return {
+            '/': { page: '/' },
+            '/about': { page: '/about' },
+            '/post': { page: '/post' },
+            '/p/[id]': { page: '/p/[id]' }
+        }
+    }
 });
